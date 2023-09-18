8f74aa786d195408bfd7bc1551ee0591cde2baec
Manchan777 <freshguni@gmail.com>
2023-09-19 06:23:49 +0900

    spring-mybatis AN ver.2.0

--- chapter05_AN/src/spring/conf/SpringConfiguration.java
+++ chapter05_AN/src/spring/conf/SpringConfiguration.java
@@ -1,14 +1,21 @@
 package spring.conf;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @PropertySource("classpath:spring/db.properties") // (복수형이면 @PropertySources로 사용)
-@Mapper
 public class SpringConfiguration {
 	// 3번 방식
 	@Value("${jdbc.driver}") // 변수에 Value(초기값) 입력 - 주의) import시 spring로 해야함 lombok 아님
@@ -18,6 +25,7 @@ public class SpringConfiguration {
 	private @Value("${jdbc.username}") String username;
 	private @Value("${jdbc.password}") String password;
 	
+	// dataSource 빈 설정
 	@Bean
 	public BasicDataSource dataSource() {  // 메소드()명은 리턴클래스의 아이디명으로 해줘야함(일종의 약속)
 		// 3번방식 (2번 방식인 네임스페이스 방식은 여기서 쓸 필요x)
@@ -32,6 +40,26 @@ public class SpringConfiguration {
 	}
 	// xml의 <bean id="dataSource" class="org.apache.commons.dbcp2.BasicDataSource"> 와 동일
 	
+	// sqlSessionFactory 빈 설정
 	@Bean
-	public
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setConfigLocation(new ClassPathResource("spring/mybatis-config.xml"));
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:user/dao/userMapper.xml"));
+        return sessionFactory;
+    }
+	
+	// SqlSession 빈 설정
+    @Bean
+    public SqlSessionTemplate sqlSession(SqlSessionFactoryBean sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory.getObject());
+    }
+    
+    // Transaction 빈 설정
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+ 
 }
--- chapter05_AN/src/user/dao/UserDAOMybatis.java
+++ chapter05_AN/src/user/dao/UserDAOMybatis.java
@@ -1,37 +1,30 @@
 package user.dao;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import lombok.Setter;
 import user.bean.UserDTO;
 
 @Component
 @Transactional // 하나씩 지정하기 귀찮으면 이렇게 해도 됨
 public class UserDAOMybatis implements UserDAO {
-
-	
 	/* applicationContext.xml에서 SqlSessionFactory설정을 해줬기때문에 여기선 필요x */
 	
-	//@Transactional // commit, close까지 자동으로 해줌(xml에서 설정해준 것을 어노테이션으로 선언함)
+	private final SqlSession sqlSession;
+	
+	@Autowired
+	public UserDAOMybatis(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
 	@Override
 	public void write(UserDTO userDTO) {
-		SqlSession sqlSession = sqlSessionFactory.openSession(); // 생성 (SqlSession는 인터페이스이므로 직접 new가 불가능)
 		sqlSession.insert("userSQL.write", userDTO); // (namespace.mapperId, 자료형)
-		sqlSession.commit();
-		sqlSession.close();
-		
-		//sqlSession.insert("userSQL.write", userDTO);
 	}
 
 	@Override
